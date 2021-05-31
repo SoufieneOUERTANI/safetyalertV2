45915d28ee3baf485532b008712aef31fbd8d090
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-05-31 13:19:25 +0200

    Init

---
+++ src/main/java/com/ouertani/safetyalertV2/SafetyalertV2Application.java
@@ -0,0 +1,13 @@
+package com.ouertani.safetyalertV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyalertV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyalertV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ouertani/safetyalertV2/SafetyalertV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouertani.safetyalertV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyalertV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
